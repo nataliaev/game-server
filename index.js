ef95a231251e5ad4e717cc3a168b73d01003858d
nataliaev <nataliiaevchenko@gmail.com>
2019-08-13 13:55:17 +0200

    Add JWT

--- index.js
+++ index.js
@@ -212,8 +212,11 @@ app.post('/logins', (req, res) => {
         if (bcrypt.compareSync(req.body.password, entity.password)) {
 
           // 3. if the password is correct, return a JWT with the userId of the user (user.id)
-          res.send( {jwt: toJWT({ userId: entity.id })}
-          )
+
+          console.log(entity)
+          res.send( { 
+            jwt: toJWT({ userId: entity.id }),
+          })
         }
         else {
           res.status(400).send({
